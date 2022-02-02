cd5247eacdc8f22061da52bb32eef0c972811f95
dashrathh043 <dashrath043soni@gmail.com>
2022-02-02 23:38:36 +0530

    com-1

---
+++ chapter-1/script.js
@@ -0,0 +1,15 @@
+let js = 'me';
+if (js === 'me') alert('Welcome!');
+console.log(45 + 56);
+let myFirstJob = 'Developer';
+let myCurrentJob = 'Teaching';
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+let country = 'India'
+let continent = 'Asia'
+let population = 'ONE BILLION THREE HUNDRED EIGHTY MILLION FOUR THOUSAND THREE HUNDRED EIGHTY-FIVE'
+
+console.log('country = ' + country)
+console.log('continent = ' + continent)
+console.log('population = ' + population)
