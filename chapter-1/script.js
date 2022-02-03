101b2784db8d052c5ddadfca14a93141a2374059
dashrathh043 <dashrath043soni@gmail.com>
2022-02-03 18:59:24 +0530

    js-updated-chp-1

--- chapter-1/script.js
+++ chapter-1/script.js
@@ -6,10 +6,21 @@ let myCurrentJob = 'Teaching';
 console.log(myFirstJob);
 console.log(myCurrentJob);
 
+//Declare a new variable
 let country = 'India'
 let continent = 'Asia'
 let population = 'ONE BILLION THREE HUNDRED EIGHTY MILLION FOUR THOUSAND THREE HUNDRED EIGHTY-FIVE'
 
+//writing a variable
 console.log('country = ' + country)
 console.log('continent = ' + continent)
 console.log('population = ' + population)
+
+console.log(true)
+let yourAge = 12
+console.log(typeof yourAge)
+console.log(yourAge)
+
+// updating a variable
+yourAge = 36
+console.log(yourAge)
